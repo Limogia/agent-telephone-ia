291a68bd7639e8ebf479cd43382d9af053afaab0
Limogia <limogia.gestionpro@gmail.com>
2026-02-25 13:07:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Railway OK");
+// IMPORTANT
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Listening on " + PORT);
 });
 
-app.listen(PORT, () => {
-  console.log("Listening on " + PORT);
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
 });
