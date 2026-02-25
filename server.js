763bdcc82edca6092ec458d15ba7e18cf6970c34
Limogia <limogia.gestionpro@gmail.com>
2026-02-25 13:01:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(express.urlencoded({ extended: false }));
-
-app.post("/voice", (req, res) => {
-  console.log("📞 Twilio a appelé");
-
-  res.set("Content-Type", "text/xml");
-  res.status(200).send(`
-<Response>
-  <Say voice="alice" language="fr-FR">
-    Serveur Railway fonctionnel.
-  </Say>
-</Response>
-  `);
-});
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("OK");
+  res.send("Railway OK");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, () => {
+  console.log("Listening on " + PORT);
 });
