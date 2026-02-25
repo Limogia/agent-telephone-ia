ff33ba1a6430f84eefb91f79604376d18991dce9
Limogia <limogia.gestionpro@gmail.com>
2026-02-25 13:22:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.post("/voice", (req, res) => {
-  res.set("Content-Type", "text/xml");
-  res.status(200).send(`
-<Response>
-  <Say voice="alice" language="fr-FR">
-    Serveur Railway stable.
-  </Say>
-</Response>
-  `);
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Listening on " + PORT);
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log("Listening on " + process.env.PORT);
 });
