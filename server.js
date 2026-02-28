89232a2ed0a7730432ea7d11b491f85e1b9630a7
Limogia <limogia.gestionpro@gmail.com>
2026-02-28 16:56:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -228,7 +228,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Serveur demarre sur le port " + PORT);
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Serveur démarré sur le port " + PORT);
-});
